3dcf58c8163585e1b033e6ecf3c9abcfa4db2c7e
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2015-12-09 15:51:48 +0530

    Doh! Removed the url route for about now.

--- src/Student_Council_Website/urls.py
+++ src/Student_Council_Website/urls.py
@@ -8,7 +8,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomePage.as_view(), name='home'),
-    url(r'^about/$', views.AboutPage.as_view(), name='about'),
     url(r'^users/', include(profiles.urls, namespace='profiles')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(accounts.urls, namespace='accounts')),
