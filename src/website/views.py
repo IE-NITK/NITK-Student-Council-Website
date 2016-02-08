c35e83f63e0dec551e7a827fb0f6ca890de66335
vidit23 <viditvineet@hotmail.com>
2016-02-08 22:12:23 +0530

    Events will show now

--- src/website/views.py
+++ src/website/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import *
-
-
+import simplejson
+from django.core.serializers import DjangoJSONEncoder
 # Create your views here.
 
-def calendar(request):
-    return render(request, 'calendar.html', {'events': Events.objects.values('title','start','end')})
+def calEvents(request):
+    events = []
+    for event in Events:
+        events.append({'title': event.title, 'start': event.start, 'end': event.end})
+    return render(request, 'calendar.html', simplejson.dumps(events, cls=DjangoJSONEncoder))
