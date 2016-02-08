04b58b1809dd8c570ed07d8d8eab354fccd7fa31
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-02-08 23:39:50 +0530

    Calendar Working!!! :D

--- src/website/views.py
+++ src/website/views.py
@@ -5,12 +5,15 @@ from django.core import serializers
 import json
 # Create your views here.
 
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
 def calEvents(request):
     #events = []
     #eventlist = Events.objects.all()
     #for event in eventlist:
     #    events.append({'title': event.title, 'start': event.start, 'end': event.end})
     #return render(request, 'calendar.html', json.dumps(events, cls=DjangoJSONEncoder))
-    objectQuerySet = Events.objects.only('title','start','end')
-    events = serializers.serialize('json', list(objectQuerySet))
+    objectQuerySet = Events.objects.values('title','start','end')
+    events = json.dumps(list(objectQuerySet), default=date_handler)
     return render(request, 'calendar.html', {'eventlist':events})
