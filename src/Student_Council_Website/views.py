e2008b7ceaedd5b923b80d28726585f56931fa92
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2016-02-08 21:09:27 +0530

    Clean up

--- src/Student_Council_Website/views.py
+++ src/Student_Council_Website/views.py
@@ -4,15 +4,5 @@ from django.views import generic
 class HomePage(generic.TemplateView):
     template_name = "home.html"
 
-
-class AboutPage(generic.TemplateView):
-    template_name = "about.html"
-
-class clublife(generic.TemplateView):
-    template_name = "clublife.html"
-
 class represent(generic.TemplateView):
     template_name = "represent.html"
-
-class events(generic.TemplateView):
-	template_name= "events.html"
