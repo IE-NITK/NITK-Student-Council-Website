5d5662e716decaa630fc3e7db70d7848b0f7fe6c
vidit23 <viditvineet@hotmail.com>
2016-02-08 23:28:19 +0530

    Calendar - WIP

--- src/Student_Council_Website/urls.py
+++ src/Student_Council_Website/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^represent/', views.represent.as_view(), name='represent'),
     url(r'^users/', include(profiles.urls, namespace='profiles')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^calendar/$', websiteviews.calendar, name='calendar'),
+    url(r'^calendar/$', websiteviews.calEvents),
     url(r'^', include(accounts.urls, namespace='accounts')),
     url(r'^markdown/', include('django_markdown.urls')),
     url(r'^clublife/', views.ClubLifePage.as_view(), name='club'),
