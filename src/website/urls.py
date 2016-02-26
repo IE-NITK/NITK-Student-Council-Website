f2e359bdfe25391942e7d015bb4eb4a2269ec656
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-02-26 19:06:06 +0530

    Pushing URLs - Missing from last commit

---
+++ src/website/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.homePage, name='home'),
+    url(r'^message/$', views.MessagePage.as_view(), name='message'),
+    url(r'^represent/', views.represent.as_view(), name='represent'),
+    url(r'^calendar/$', views.calEvents, name='calendar'),
+    url(r'^clublife/', views.ClubLifePage.as_view(), name='club'),
+    url(r'^announce/', views.announcements, name='announce'),
+    url(r'^contacts/',views.ContactNumbers.as_view(),name='contacts'),
+]
