34610e7c7bfcb76e56ac2031f3e76fa5e4551da2
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-04-07 01:36:22 +0530

    WIP: Smriti - Login and Search implemented

--- src/smriti/urls.py
+++ src/smriti/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexPage.as_view(), name='index'),
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^home/', views.HomePage.as_view(), name='home'),
+    url(r'^$', views.indexPage, name='index'),
+    url(r'^home/', views.homePage, name='home'),
     url(r'^browse/', views.BrowsePage.as_view(), name='browse'),
-    url(r'^search/', views.SearchPage.as_view(), name='search'),
+    url(r'^search/', views.searchPage, name='search'),
     url(r'^testimonial/', views.TestimonialPage.as_view(), name='testimonial'),
     ]
