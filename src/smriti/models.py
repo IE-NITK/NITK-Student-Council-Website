68dcd3c036a1ff7422fdb926ba9a4034e40db745
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-04-07 01:36:22 +0530

    WIP: Smriti - Login and Search implemented

--- src/smriti/models.py
+++ src/smriti/models.py
@@ -5,10 +5,10 @@ from django.conf import settings
 # Create your models here.
 
 class Testimonial(models.Model):
-    description = MarkdownField()
     testimonial_to = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="receiver")
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name="sender")
     timestamp = models.DateTimeField(auto_now_add=True)
+    description = MarkdownField()
 
     def __str__(self):
-        return "%s to %s" % (created_by,testimonial_to)
+        return "%s to %s" % (self.created_by.get_full_name(),self.testimonial_to.get_full_name())
--- src/smriti/views.py
+++ src/smriti/views.py
@@ -2,30 +2,41 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib import auth
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from profiles.models import Profile
+from .models import *
 
 # Create your views here.
-class IndexPage(generic.TemplateView):
-    template_name = "smriti/index.html"
 
-def login_view(request):
+def indexPage(request):
     if request.method == 'POST':
         username = request.POST.get('email','')
         password = request.POST.get('password','')
         user = auth.authenticate(email=username,password=password)
-
         if user is not None:
             return HttpResponseRedirect('/smriti/home/')
         else:
             return HttpResponseRedirect('/smriti/invalid/')
+    if request.method == 'GET':
+        return render(request,'smriti/index.html')
 
-class HomePage(generic.TemplateView):
-    template_name = "smriti/home.html"
+@login_required
+def homePage(request):
+    print request.user
+    testimonials = Testimonial.objects.filter(testimonial_to=request.user)
+    return render(request,"smriti/home.html",{'testimonials':testimonials})
 
 class BrowsePage(generic.TemplateView):
     template_name = "smriti/browse.html"
 
-class SearchPage(generic.TemplateView):
-    template_name = "smriti/search.html"
+def searchPage(request):
+    if request.method == 'POST':
+        search_param = request.POST.get('search_param','')
+        name_result = Profile.objects.filter(user__name__icontains=search_param)
+        roll_result = Profile.objects.filter(rollno__iexact=search_param)
+        return render(request,"smriti/search.html",{'name_result':name_result,'roll_result':roll_result})
+    if request.method == 'GET':
+        return render(request,'smriti/search.html')
 
 class TestimonialPage(generic.TemplateView):
     template_name = "smriti/testimonial.html"
